4c2ed9c0d8d23817854ff541651e3aa766292a2c
wicos <marcin.machaa@gmail.com>
2023-03-17 11:58:01 +0100

    Initial commit

---
+++ src/Obliczenia.java
@@ -0,0 +1,11 @@
+public class Obliczenia {
+
+    public static int dodaj(int a, int b) {
+        return a + b;
+    }
+
+    public static void wynik(int a, int b){
+        System.out.println(dodaj(a,b));
+    }
+
+}
